5a132f91189658c9ee9becd6360f2fa91338fceb
Lars Dahl <laerdh@gmail.com>
2019-10-26 13:11:26 +0200

    Create custom oauth approval page

--- src/main/kotlin/com/ledahl/services/authservice/config/controller/ClientAuthorizationController.kt
+++ src/main/kotlin/com/ledahl/services/authservice/config/controller/ClientAuthorizationController.kt
@@ -1,6 +1,8 @@
 package com.ledahl.services.authservice.config.controller
 
+import com.ledahl.services.authservice.model.CustomUserDetail
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.provider.AuthorizationRequest
 import org.springframework.security.oauth2.provider.ClientDetailsService
 import org.springframework.stereotype.Controller
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttributes
 import org.springframework.web.servlet.ModelAndView
 import java.util.*
 
+
 @Controller
 @SessionAttributes("authorizationRequest")
 class ClientAuthorizationController(@Autowired private val clientDetailsService: ClientDetailsService) {
@@ -18,9 +21,13 @@ class ClientAuthorizationController(@Autowired private val clientDetailsService:
     @Throws(Exception::class)
     fun getAccessConfirmation(@ModelAttribute clientAuth: AuthorizationRequest): ModelAndView {
         val client = clientDetailsService.loadClientByClientId(clientAuth.clientId)
-        val model = TreeMap<String, Any>()
+        val userDetail = SecurityContextHolder.getContext().authentication.principal as? CustomUserDetail
+        val model = TreeMap<String, Any?>()
         model["auth_request"] = clientAuth
-        model["client"] = client
+        model["client_id"] = client.clientId
+        model["username"] = userDetail?.user?.email
+        model["additional_information"] = client.additionalInformation
+
         return ModelAndView("confirm_access", model)
     }
 }
